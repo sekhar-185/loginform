2954556c0775a7fc8c4b70a98c62f50c1ecb736f
sekhar <sekhar18505@gmail.com>
2025-01-12 17:17:55 +0530

    add login form

---
+++ index.js
@@ -0,0 +1,10 @@
+const exp = require("constants");
+const express=require("express");
+const app=express();
+
+app.use(express.static("./public"));
+
+app.listen(3000,()=>{
+    console.log("listenig on port.");
+})
+
